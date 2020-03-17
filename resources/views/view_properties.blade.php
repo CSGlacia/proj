b8436ada2e60bb6f488a93f5d96f02a9dbfbd15b
jun-lin1 <36977477+jun-lin1@users.noreply.github.com>
2020-03-17 21:38:07 +1100

    whywonthiswork

--- resources/views/view_properties.blade.php
+++ resources/views/view_properties.blade.php
@@ -7,13 +7,13 @@
         <form action="/view_property" method="GET"> <!--CHANGE TO SEARCH LOGIC-->
           <input type="text" placeholder="Search by name..." name="query" size="100">
           <input type="submit" value="Search" />
+          <input type="checkbox" name="address_checkbox" value="1">
+          <label for="address_checkbox"> Address </label><br>
+          <input type="checkbox" name="suburb_checkbox" value="1">
+          <label for="suburb_checkbox"> Suburb </label><br>
+          <input type="checkbox" name="postcode_checkbox" value="1">
+          <label for="postcode_checkbox"> Postcode </label><br>
         </form>
-        <input type="checkbox" name="address_checkbox" value="1" checked>
-        <label for="address_checkbox"> Address </label><br>
-        <input type="checkbox" name="suburb_checkbox" value="1" checked>
-        <label for="suburb_checkbox"> Suburb </label><br>
-        <input type="checkbox" name="postcode_checkbox" value="1" checked>
-        <label for="postcode_checkbox"> Postcode </label><br>
 
       </div>
 
