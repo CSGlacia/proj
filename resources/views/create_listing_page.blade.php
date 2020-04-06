fa90fb00b608c7980040280f8870bc23a18b08b8
jun-lin1 <36977477+jun-lin1@users.noreply.github.com>
2020-04-06 12:13:27 +1000

    nav bar

---
+++ resources/views/create_listing_page.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card col-sm-12 col-md-12 col-lg-12">
+        <br>
+        <b><h3 style="text-align:center;">Create a New Property Listing</h3></b>
+        <hr>
+        <div id="listing_form" class="form-group">
+            <h5>Location Details:&nbsp;</h5>
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                    <span>Address:&nbsp;</span>
+                    <input id="address" class="form-control" type="text" placeholder="E.g. 6/5 George Street" required>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                    <span>Suburb:&nbsp;</span>
+                    <input id="suburb" class="form-control" type="text" placeholder="E.g. Ryde" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-6">
+                    <span>Postcode:&nbsp;</span>
+                    <input id="postcode" class="form-control" type="number" placeholder="E.g. 2225" required>
+                </div>
+            </div>
+            <hr>
+            <h5>Property Details:&nbsp;</h5>
+            <div class="row">
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <span># of Bedrooms:&nbsp;</span>
+                    <input id="beds" class="form-control" type="number" placeholder="0" required>
+                </div>
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <span># of Bathrooms:&nbsp;</span>
+                    <input id="baths" class="form-control" type="number" placeholder="0" required>
+                </div>
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <span># of Car Spaces:&nbsp;</span>
+                    <input id="cars" class="form-control" type="number" placeholder="0" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <textarea id="property_desc" class="form-control" rows="5" placeholder="Please enter a brief description of the property." required></textarea>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <a id="property_submit" class="btn btn-primary">Submit</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<script>
+$(document).ready(function() {
+    $(document).on('click', '#property_submit', function(e) {
+        e.preventDefault();
+
+        var address = $('#address').val();
+        var suburb = $('#suburb').val();
+        var postcode = $('#postcode').val();
+        var beds  = $('#beds').val();
+        var baths = $('#baths').val();
+        var cars = $('#cars').val();
+        var desc = $('#property_desc').val();
+
+        $.ajax({
+            url: '/create_property',
+            method: 'POST',
+            data: 'address='+address+'&suburb='+suburb+'&postcode='+postcode+'&beds='+beds+'&baths='+baths+'&cars='+cars+'&desc='+desc,
+            success: function(html) {
+                var data = tryParseJSON(html);
+
+                if(data['status'] == "success") {
+                    alert("Property Created Successfully");
+                } else if(data['status'] == 'bad_input') {
+                    alert("Please double check all fields are filled!");
+                } else {
+                    alert("There was an error, please try again!");
+                }
+            },
+            error: function ( xhr, errorType, exception ) {
+                var errorMessage = exception || xhr.statusText;
+                alert("There was a connectivity problem. Please try again.");
+            }
+        });
+    });
+});
+</script>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/nav_bar.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -35,7 +36,7 @@
     .item-card:hover {
         background-color: #f6f6f6;
         box-shadow: inset 0 0 0 5px #c9c9c9;
-        transition: all 1s ease;  
+        transition: all 1s ease;
     }
     .item-card {
         background-color: #ffffff;
@@ -53,6 +54,65 @@
 
 </style>
 @yield('style')
+
+<section class="header">
+            <div class="overlay">
+                <div class="row">
+                   <div class="navbar navbar-default">
+                      <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                            <div class="navlogo col-lg-2">
+                                <a class="navbar-brand" href="#">Name</a>
+                            </div>
+
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                <div class="col-lg-8">
+                                      <ul class="nav navbar-nav nav-links">
+                                         <li><a href="#">Demos</a></li>
+                                        <li><a href="#">Featured</a></li>
+                                        <li><a href="#">Pages</a></li>
+                                        <li><a href="#">Elements</a></li>
+                                        <li><a href="#">Portfolio</a></li>
+                                        <li><a href="#">Blog</a></li>
+                                        <li><a href="#">Shop</a></li>
+
+                                      </ul>
+                                    </div>
+
+                                <div class="col-lg-2 right">
+                                     <ul class="nav navbar-nav navbar-right">
+                                        <li><a href="#"><i class="fa fa-search fa-2x"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-shopping-cart fa-2x"></i></a></li>
+
+                                    </ul>
+                                </div>
+
+                            </div>
+
+
+                      </div>
+                 </div>
+
+
+                </div>
+
+
+
+
+
+            </div>
+        </section>
 
 
 
@@ -72,10 +132,9 @@
                     <a class="btn btn-xs btn-success"href="/tennant_reviews">Review past tennants</a>
                     <a class="btn btn-xs btn-danger" href="/create_property_page">Add a new property to your account</a>
                 @else
-                    <a class="btn btn-xs btn-primary" href="{{ route('login') }}">Login</a>
-
+                    <a class="btn btn-xs btn-primary float-right" href="{{ route('login') }}    ">Login</a>
                     @if (Route::has('register'))
-                        <a class="btn btn-xs btn-primary" href="{{ route('register') }}">Register</a>
+                        <a class="btn btn-xs btn-primary float-right" href="{{ route('register') }}">Register</a>
                     @endif
                 @endauth
             </div>
