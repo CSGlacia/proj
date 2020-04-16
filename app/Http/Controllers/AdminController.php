6efa7d1b82de960c8821ad5d81a57932ceb931f2
AndyLiu11037 <32506371+AndyLiu11037@users.noreply.github.com>
2020-04-16 13:26:01 +1000

    adding view all bookings

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,9 +20,35 @@ class AdminController extends Controller{
     }
 
     public function all_bookings(Request $request){
-        $results = DB::table('bookings AS c')
+        $bookings = DB::table('bookings AS c')
                     ->select('c.*')
+                    ->where([['b.booking_inactive', 0],
+                            ['b.booking_endDate', '>', time()])
                     ->get();
+        $results = [];
+        foreach($bookings as $booking){
+            $result = [];
+            $username = DB::table('users as u')
+                            ->select('u.name')
+                            ->where('u.id', $booking->booking_userID)
+                            ->get();
+            array_push($result,$username->name);
+
+            $title = DB::table('properties as p')
+                            ->select('p.title')
+                            ->where('p.property_id', $booking->booking_propertyID)
+                            ->get();
+            array_push($result,$title->title);
+            array_push($result,$booking->booking_id);
+            array_push($result,$booking->booking_startDate);
+            array_push($result,$booking->booking_endDate);
+
+            array_push($results,$result);
+        }
+        return view('view_all_bookings',
+        [
+            'results' => $results,
+        ]);
     }
 
     public function all_reviews(Request $request){
---
+++ resources/views/view_all_bookings.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @foreach ($results as $r)
+      <hr>
+      <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12">
+              <h3><b> {{ $r->booking_id }}</b></h3>
+              <div> {{ $w->wishlist_propertyAddress }}  </div>
+              <a class="btn btn-primary" name="view_property" href="/view_property/{{$w->wishlist_propertyID}}"> View property </a>
+            <!--  <a id="delete_wishlist <?php echo $w->wishlist_propertyID ?>" class="btn btn-primary">✖</a> -->
+              <input class ="delete_wishlist" type="button" id="<?php echo $w->wishlist_propertyID?>", value = "✖" </input>
+          </div>
+      </div>
+    @endforeach
+
+</div>
+@endsection
+
+@section('scripts')
+<script>
+$(document).ready(function() {
+    $(document).on('click', '.delete_wishlist', function(e) {
+        e.preventDefault();
+        var propertyID = $(this).get(0).id;
+        alert(propertyID);
+
+        $.ajax({
+            url: '/delete_wishlist',
+            method: 'POST',
+            dataType: 'JSON',
+            data: 'propertyID='+propertyID,
+            success: function(html) {
+                var data = JSON.parse(html);
+                if(data['status'] == "success") {
+                  Swal.fire("Success", "Removed this item from your wishlist!", "success");
+                } else {
+                    Swal.fire("Error", "There was an error, please try again!", "error");
+                }
+            },
+            error: function ( xhr, errorType, exception ) {
+                var errorMessage = exception || xhr.statusText;
+                Swal.fire("Error", "There was a connectivity problem. Please try again.", "error");
+            }
+        });
+
+    });
+});
+</script>
+@endsection
