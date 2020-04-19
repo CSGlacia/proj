6c1c481a46fe96d225b3645f0b22433a31bfa97d
AndyLiu11037 <32506371+AndyLiu11037@users.noreply.github.com>
2020-04-19 18:56:34 +1000

    Update create_property_listing.blade.php

--- resources/views/create_property_listing.blade.php
+++ resources/views/create_property_listing.blade.php
@@ -23,24 +23,14 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-auto">
-                <label for="form_property_list_start_date">Start Date:</label>
-                    <input id="form_property_list_start_date" class="form-control" type="date" required>
-                </div>
-                <div class="col-auto">
-                <label for="form_property_list_end_date">End Date:</label>
-                    <input id="form_property_list_end_date" class="form-control" type="date" required>
-                </div>
-            </div>
             <h5>Listing Dates:&nbsp;</h5>
             <span id="dates_start">
                 <div class="row listing_dates" name="listing_dates">
-                    <div class="col-sm-3 col-md-3 col-lg-3">
+                    <div class="col-sm-2 col-md-2 col-lg-2">
                         <span>Start Date:&nbsp;</span>
                         <input class="form-control" id="first_start_date" name="start_date" type="text" required>
                     </div>
-                    <div class="col-sm-3 col-md-3 col-lg-3">
+                    <div class="col-sm-2 col-md-2 col-lg-2">
                         <span>End Date:&nbsp;</span>
                         <input class="form-control" id="first_end_date" name="end_date" type="text" required>
                     </div>
@@ -54,8 +44,10 @@
                     </div>
                 </div>
             </span>
-            <div class="col-sm-6 col-md-6 col-lg-6" name="listing_dates">
-                <label class="btn btn-primary float-right" id="add_dates"><i class="fas fa-plus"></i>&nbsp;Add Dates</label>
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-6" name="listing_dates">
+                    <label class="btn btn-primary float-left" id="add_dates"><i class="fas fa-plus"></i>&nbsp;Add Dates</label>
+                </div>
             </div>
             <hr>
             <div class="row">
@@ -119,7 +111,7 @@ $(document).ready(function() {
 
     $(document).on('click', '#add_dates', function(e) {
         if(count < 5) {
-            var elem = $('#dates_start').append('<div class="row listing_dates" name="listing_dates"><div class="col-sm-3 col-md-3 col-lg-3"><span>Start Date:&nbsp;</span><input class="form-control" name="start_date" type="text" required></div><div class="col-sm-3 col-md-3 col-lg-3"><span>End Date:&nbsp;</span><input class="form-control" name="end_date" type="text" required></div><div class="col-sm-1 col-md-1 col-lg-1" style="margin-top:27px;"><div class="pretty p-default p-round p-smooth p-bigger"><input name="reccur_dates" type="checkbox" /><div class="state p-primary"><label>Set as reccuring dates</label></div></div></div><div class="col-sm-1 col-md-1 col-lg-1" style="margin-left:100px;"><label class="btn btn-danger float-left" name="remove_dates" style="margin-top:22px;"><i class="fas fa-times"></i></label></div></div>');
+            var elem = $('#dates_start').append('<div class="row listing_dates" name="listing_dates"><div class="col-sm-2 col-md-2 col-lg-2"><span>Start Date:&nbsp;</span><input class="form-control" name="start_date" type="text" required></div><div class="col-sm-2 col-md-2 col-lg-2"><span>End Date:&nbsp;</span><input class="form-control" name="end_date" type="text" required></div><div class="col-sm-1 col-md-1 col-lg-1" style="margin-top:27px;"><div class="pretty p-default p-round p-smooth p-bigger"><input name="reccur_dates" type="checkbox" /><div class="state p-primary"><label>Set as reccuring dates</label></div></div></div><div class="col-sm-1 col-md-1 col-lg-1" style="margin-left:100px;"><label class="btn btn-danger float-left" name="remove_dates" style="margin-top:22px;"><i class="fas fa-times"></i></label></div></div>');
 
 
             $('[name="start_date"]').each(function() {
@@ -150,39 +142,50 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+    var listing_dates_arr = [];
+    $('.listing_dates').each(function (i) {
+            var start_date = $(this).find('input[name="start_date"]').val();
+            var end_date = $(this).find('input[name="end_date"]').val();
+            var recur = $(this).find('input[name="reccur_dates"]').prop('checked');
+
+            var add_arr = [];
+
+            add_arr.push(start_date);
+            add_arr.push(end_date);
+            add_arr.push(recur);
+            listing_dates_arr.push(add_arr);
+    });
+
     $(document).on('click', '#property_list_submit', function(e) {
         e.preventDefault();
         var property = $('#form_select_property').val();
         var price = $('#price').val();
-        var start_date = $('#form_property_list_start_date').val();
-        var end_date = $('#form_property_list_end_date').val();
-        $.ajax({
-            url: '/create_property_listing',
-            method: 'POST',
-            dataType: 'JSON',
-            data: 'property='+property+'&price='+price+'&start_date='+start_date+'&end_date='+end_date,
-            success: function(html) {
-                if(html['status'] == "success") {
-                    alert("Property Listing created successfully");
-                } else if(html['status'] == 'bad_input'){
-                    alert("Please check all fields are filled.");
-                } else if(html['status'] == 'price_low') {
-                    alert("You must enter a price which is positive. You cannot charge negative amounts.");
-                } else if(html['status'] == 'price_high'){
-                    alert("There's a price limit of $999999.99 . Please enter a lower price per night.");
-                } else if(html['status'] == 'date_invalid'){
-                    alert("Your start date must be before your end date and today or after.")
-                } else if(html['status'] == 'overlapping_date'){
-                    alert("A listing already exists within this time period.")
+        $.each(listing_dates_arr, function(i) {
+            $.ajax({
+                url: '/create_property_listing',
+                method: 'POST',
+                dataType: 'JSON',
+                data: 'property='+property+'&price='+price+'&start_date='+listing_dates_arr[i][0]+'&end_date='+listing_dates_arr[i][1]+'&recurr='+listing_dates_arr[i][2],
+                success: function(html) {
+                    if(html['status'] == "success") {
+                        Swal.fire("Success", "Property Listing(s) Created Successfully", "success");
+                    } else if(html['status'] == 'bad_input'){
+                        Swal.fire("Error","Please check all fields are filled.","error");
+                    } else if(html['status'] == 'price_low') {
+                        Swal.fire("Error","You must enter a price which is positive. You cannot charge negative amounts.","error");
+                    } else if(html['status'] == 'price_high'){
+                        Swal.fire("Error","There's a price limit of $999999.99 . Please enter a lower price per night.","error");
+                    } else if(html['status'] == 'overlapping_date'){
+                        Swal.fire("Error","A listing already exists within this time period.","error");
+                    } else {
+                        Swal.fire("Error","There was an error with creating your listings! Please try again on the Create Listing Page", "error");
+                    }
+                },
+                error: function ( xhr, errorType, exception ) {
+                    var errorMessage = exception || xhr.statusText;
+                    Swal.fire("Error", "There was a connectivity problem. Please try again.", "error");
                 }
-                else {
-                    alert("There was an error, please try again!");
-                }
-            },
-            error: function ( xhr, errorType, exception ) {
-                var errorMessage = exception || xhr.statusText;
-                alert("There was a connectivity problem. Please try again.");
-            }
+            });
         });
     });
 });
