58b79d66c748cbf1f689135e4f9cfefeb2bb5dae
jun-lin1 <36977477+jun-lin1@users.noreply.github.com>
2020-04-21 14:23:00 +1000

    wishlist button fixed

--- resources/views/view_wishlist.blade.php
+++ resources/views/view_wishlist.blade.php
@@ -11,7 +11,7 @@
               <div> {{ $w->wishlist_propertyAddress }}  </div>
               <a class="btn btn-primary" name="view_property" href="/view_property/{{$w->wishlist_propertyID}}"> View property </a>
             <!--  <a id="delete_wishlist <?php echo $w->wishlist_propertyID ?>" class="btn btn-primary">✖</a> -->
-              <input class ="delete_wishlist btn btn-danger" type="" id="<?php echo $w->wishlist_propertyID?>", value = "Remove from wishlist" </input>
+              <a class ="delete_wishlist btn btn-danger" style="color:white"type="" id="<?php echo $w->wishlist_propertyID?>", <>Remove from wishlist</a>
           </div>
       </div>
     @endforeach
