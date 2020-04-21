9e9a78ac479e8a9a9a44a336b3f79f5e8d8164d3
jun-lin1 <36977477+jun-lin1@users.noreply.github.com>
2020-04-21 17:30:57 +1000

    admin adv table

--- resources/views/admin_advertiser.blade.php
+++ resources/views/admin_advertiser.blade.php
@@ -4,11 +4,29 @@
 <div class=container style="background:rgba(240,255,248,0.6)">
     <h1>All Users</h1>
     <hr>
-    <div class="container row" style="margin:auto">
+    <table>
+        <tr>
+            <th style="padding:10px">Username</th>
+            <th style="padding:50px">ID</th>
+            <th style="padding:10px">Make Advertiser</th>
+        </tr>
+        @foreach ($users as $u)
+          <tr>
+              <td style="padding-left:10px">{{$u->name}}</td>
+              <td style="padding-left:50px">{{$u->id}}</td>
+              <td style="padding-left:10px">@if ($advertisers[$u->id] == False)
+                  <a class="btn btn-primary"  style="margin:5px; color:white" name="change_user" data-id="{{$u->id}}"> Make Advertiser</a>
+              @else
+                  <a class="btn btn-primary"  style="margin:5px; color:white" name="change_user" data-id="{{$u->id}}"> Remove Advertiser status</a>
+              @endif</td>
+            </tr>
+        @endforeach
+    </table>
+    <!--<div class="container row" style="margin:auto">
         @foreach ($users as $u)
           <hr>
           <div class="row" align="center">
-              <div class="col-sm-9 col-md-9 col-lg-9 card" style="margin:0px; width:20vw;">
+              <div class="col-sm-12 col-md-12 col-lg-12 card" style="margin:0px;">
               <h5>Username: {{$u->name}}</h5>
                 <h6> ID: {{$u->id}}</h6>
                 @if ($advertisers[$u->id] == False)
@@ -16,13 +34,13 @@
                 @else
                     <a class="btn btn-primary"  style="margin:5px; color:white" name="change_user" data-id="{{$u->id}}"> Remove Advertiser status</a>
                 @endif
-                    
+
               </div>
           </div>
         @endforeach
 
     </div>
-</div>
+</div>-->
 @endsection
 
 @section('scripts')
