61c16a620bdf1b2631b719c2963ced794fedc4a1
AndyLiu11037 <32506371+AndyLiu11037@users.noreply.github.com>
2020-04-21 17:50:26 +1000

    Update view_booking.blade.php

--- resources/views/view_booking.blade.php
+++ resources/views/view_booking.blade.php
@@ -25,7 +25,7 @@
             <div>Status: {{$status}}</div>
         </div>
     </div>
-    @if($user_id == $b->id && $status == 'NOT APPROVED')
+    @if($user_id == $tennant->id && $status == 'NOT APPROVED')
         <div class="row" style="margin-top:5px;"><span class="btn btn-success" name="approve_booking" data-id="{{$b->booking_id}}">Approve Booking</span>&nbsp;<span class="btn btn-warning" name="deny_booking" data-id="{{$b->booking_id}}">Deny Booking</span></div>
     @endif
 </div>
